b6daa6e78d9090f538ab8ec4246f0fa4c6fc9f8c
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-10 17:41:59 +0530

    Remove boilerplate code

---
+++ src/components/Editor/Editor.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Editor() {
+    return (
+        <h1>editor</h1>
+    );
+}
+
+export default Editor;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Editor from "../Editor/Editor";
+
+function Home() {
+    return (
+        <div className="home">
+            <Editor />
+        </div>
+    );
+}
+
+export default Home;
