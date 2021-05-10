00a28550f840466cd88df52dba76f5b59a0381d7
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-10 18:45:45 +0530

    Add Editor content

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,8 +1,37 @@
 import React from "react";
+import { TextEditor } from "text-editor-react";
+
+import "./Editor.js";
+
+const styles = {
+    toolbarStyle: {
+        display: "flex",
+        alignItems: "center",
+        justifycontent: "center",
+    },
+    toolItemStyle: {
+        // color: "#56CFDA",
+        // backgroundColor: "white",
+    },
+};
 
 function Editor() {
+    const id = "text-editor-demo-id";
+    const targetDivId = "target-div-id";
+
     return (
-        <h1>editor</h1>
+        <div className="editor">
+            <i className="fas fa-redo"></i>
+            <TextEditor
+                id={id}
+                toolbarStyle={styles.toolbarStyle}
+                toolItemStyle={styles.toolItemStyle}
+            />
+            <div className="display-content">
+                <h2 className="target-heading">Target Div</h2>
+                <div className="target-div" id={targetDivId}></div>
+            </div>
+        </div>
     );
 }
 
